60459aed35a004a02ed354ced7fe7d119b712235
jonatas ribeiro <jonatas@prottaribeiro.com>
2023-10-25 21:59:57 +0100

    simple linear regression

--- FICHAS/FP03/fp_03.py
+++ FICHAS/FP03/fp_03.py
@@ -63,3 +63,58 @@ plt.scatter(cdf.CYLINDERS, cdf.CO2EMISSIONS, color="black")
 
 #TRAINING DATASET AND SPLITING
 
+np.random.seed(0) #set the seed to obtain the same random values
+
+rand_samp = np.random.rand(len(df))
+
+plt.hist(rand_samp, bins=100)
+
+np.random.seed(0)
+msk = np.random.rand(len(df)) < 0.8 #applying a mask to get 80% ofo the values to train
+
+for i in range(100): print("mask",msk[i])
+
+train = cdf[msk] #apply filter to get train samples
+test = cdf[~msk] #get test samples using ~ to negate
+
+#chekck
+
+train
+test
+
+
+print("percentage", len(train)/(len(train)+len(test)))
+
+#visualize training data
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color= "blue")
+plt.xlabel("Engine size")
+plt.ylabel("CO2 Emissions")
+plt.show()
+
+#using sklearn package to model data
+
+from sklearn import linear_model
+
+regr = linear_model.LinearRegression()
+
+train_x = np.asanyarray(train[["ENGINESIZE"]])
+# train_x = np.array(train[["ENGINESIZE"]])
+train_y = np.asanyarray(train[["CO2EMISSIONS"]])
+
+regr.fit(train_x, train_y)
+
+#COEFICCIENTS
+print("COEFICIENTS", regr.coef_) #teta1
+print("INTERCEPT", regr.intercept_) #teta0
+
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS, color="red")
+
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0],"-r", linewidth=3)
+
+plt.xlabel("ENGINE SIZE")
+plt.ylabel("EMISSION")
+plt.title("LINEAR REGRESSION - TRAIN")
+
+exResult = regr.intercept_ + (regr.coef_*(2.9))
+print("EXERCISE RESULT:",exResult)
