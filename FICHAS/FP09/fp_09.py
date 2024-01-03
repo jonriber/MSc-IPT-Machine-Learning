8be5ecddbd7041df5d8966676515179435fcbb75
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-01-03 21:06:46 +0000

    polynomial_svm_clf max iteration

--- FICHAS/FP09/fp_09.py
+++ FICHAS/FP09/fp_09.py
@@ -131,7 +131,7 @@ from sklearn.preprocessing import PolynomialFeatures
 polynomial_svm_clf = Pipeline([
  ("poly_features", PolynomialFeatures(degree=3)),
  ("scaler", StandardScaler()),
- ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42, max_iter=20))
+ ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42, max_iter=20000))
  ])
 polynomial_svm_clf.fit(X, y)
 
