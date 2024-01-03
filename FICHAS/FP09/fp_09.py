db005c253baad0191764534c158dadf0abc56c83
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-01-03 21:05:45 +0000

    fp_09 exec

--- FICHAS/FP09/fp_09.py
+++ FICHAS/FP09/fp_09.py
@@ -105,3 +105,73 @@ grid_search.cv_results_
 grid_search.best_score_
 grid_search.best_estimator
 
+# y_est = grid_search.best_estimator_.predict(X)
+# balanced_accuracy_score(y, y_est)
+
+
+#%% #dataset MakeMoons #################
+from sklearn.datasets import make_moons
+X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+#%%
+def plot_dataset(X, y, axes):
+    plt.plot(X[:, 0][y==0], X[:, 1][y==0], "bs")
+    plt.plot(X[:, 0][y==1], X[:, 1][y==1], "g^")
+    plt.axis(axes)
+    plt.grid(True, which='both')
+    plt.xlabel(r"$x_1$", fontsize=20)
+    plt.ylabel(r"$x_2$", fontsize=20, rotation=0)
+ 
+#%%
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+
+#%%
+from sklearn.preprocessing import PolynomialFeatures
+
+polynomial_svm_clf = Pipeline([
+ ("poly_features", PolynomialFeatures(degree=3)),
+ ("scaler", StandardScaler()),
+ ("svm_clf", LinearSVC(C=10, loss="hinge", random_state=42, max_iter=20))
+ ])
+polynomial_svm_clf.fit(X, y)
+
+#%%
+def plot_predictions(clf, axes):
+    x0s = np.linspace(axes[0], axes[1], 100)
+    x1s = np.linspace(axes[2], axes[3], 100)
+    x0, x1 = np.meshgrid(x0s, x1s)
+    X = np.c_[x0.ravel(), x1.ravel()]
+    y_pred = clf.predict(X).reshape(x0.shape)
+    y_decision = clf.decision_function(X).reshape(x0.shape)
+    plt.contourf(x0, x1, y_pred, cmap=plt.cm.brg, alpha=0.2)
+    plt.contourf(x0, x1, y_decision, cmap=plt.cm.brg, alpha=0.1)
+
+#%%
+plot_predictions(polynomial_svm_clf, [-1.5, 2.5, -1, 1.5])
+plot_dataset(X, y, [-1.5, 2.5, -1, 1.5])
+
+#%%
+y_predict = polynomial_svm_clf.predict(X)
+accuracy_score(y, y_predict)
+balanced_accuracy_score(y, y_predict)
+precision_score(y, y_predict)
+recall_score(y, y_predict)
+f1_score(y, y_predict)
+
+#%% EXERCISE 3
+from sklearn.svm import SVC
+
+poly_kernel_svm_clf = Pipeline([
+ ("scaler", StandardScaler()),
+ ("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+ ])
+
+poly_kernel_svm_clf.fit(X, y)
+
+
+y_predict = poly_kernel_svm_clf.predict(X)
+accuracy_score(y, y_predict)
+balanced_accuracy_score(y, y_predict)
+precision_score(y, y_predict)
+recall_score(y, y_predict)
+f1_score(y, y_predict)
