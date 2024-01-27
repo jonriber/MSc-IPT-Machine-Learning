d2aaa2a854ed3a9b2809532c3bea5b730bb82016
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-01-27 20:39:19 +0000

    part 2 - initial CNN model with tensorflow and gitIgnore update

--- PROJECTS/PROJECT_2/project2_part2.py
+++ PROJECTS/PROJECT_2/project2_part2.py
@@ -27,7 +27,7 @@ IMAGE_SIZE = (150, 150)
 
 def load_data():
     
-    datasets = ['../input/seg_train/seg_train', '../input/seg_test/seg_test']
+    datasets = ['./input/seg_train/seg_train', './input/seg_test/seg_test']
     output = []
     
     # Iterate through training and test sets
@@ -135,3 +135,91 @@ def display_examples(class_names, images, labels):
     
 #%%
 display_examples(class_names, train_images, train_labels)
+
+# %% MODEL CREATION
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu', input_shape = (150, 150, 3)), 
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(6, activation=tf.nn.softmax)
+])
+
+#%%
+model.compile(
+    optimizer = 'adam', 
+    loss = 'sparse_categorical_crossentropy', 
+    metrics=['accuracy'])
+
+#%% FIT
+history = model.fit(train_images, train_labels, batch_size=128, epochs=20, validation_split = 0.2)
+
+#%%
+def plot_accuracy_loss(history):
+    """
+        Plot the accuracy and the loss during the training of the nn.
+    """
+    fig = plt.figure(figsize=(10,5))
+
+    # Plot accuracy
+    plt.subplot(221)
+    plt.plot(history.history['acc'],'bo--', label = "acc")
+    plt.plot(history.history['val_acc'], 'ro--', label = "val_acc")
+    plt.title("train_acc vs val_acc")
+    plt.ylabel("accuracy")
+    plt.xlabel("epochs")
+    plt.legend()
+
+    # Plot loss function
+    plt.subplot(222)
+    plt.plot(history.history['loss'],'bo--', label = "loss")
+    plt.plot(history.history['val_loss'], 'ro--', label = "val_loss")
+    plt.title("train_loss vs val_loss")
+    plt.ylabel("loss")
+    plt.xlabel("epochs")
+
+    plt.legend()
+    plt.show()
+    
+#%%
+plot_accuracy_loss(history)
+#%% TEST SET
+
+test_loss = model.evaluate(test_images, test_labels)
+
+#%% PREDICTION
+
+predictions = model.predict(test_images)     # Vector of probabilities
+pred_labels = np.argmax(predictions, axis = 1) # We take the highest probability
+
+display_random_image(class_names, test_images, pred_labels)
+
+#%% MISLABELED IMAGES
+def print_mislabeled_images(class_names, test_images, test_labels, pred_labels):
+    """
+        Print 25 examples of mislabeled images by the classifier, e.g when test_labels != pred_labels
+    """
+    BOO = (test_labels == pred_labels)
+    mislabeled_indices = np.where(BOO == 0)
+    mislabeled_images = test_images[mislabeled_indices]
+    mislabeled_labels = pred_labels[mislabeled_indices]
+
+    title = "Some examples of mislabeled images by the classifier:"
+    display_examples(class_names,  mislabeled_images, mislabeled_labels)
+    
+#%%
+print_mislabeled_images(class_names, test_images, test_labels, pred_labels)
+
+#%%
+CM = confusion_matrix(test_labels, pred_labels)
+ax = plt.axes()
+sn.heatmap(CM, annot=True, 
+    annot_kws={"size": 10}, 
+    xticklabels=class_names, 
+    yticklabels=class_names, ax = ax)
+ax.set_title('Confusion matrix')
+plt.show()
+# %%
